63eb9f7b5c73908d166c07f213ddf65519b03ace
ilopit <lopit.i.i@gmail.com>
2026-05-17 09:38:53 +0200

    Replace assert() with automation-friendly assert handler
    Log + flush via spdlog, break if debugger attached, TerminateProcess
    to exit without CRT modal dialog.
    
    Co-Authored-With-AI

---
+++ libs/utils/private/src/check.cpp
@@ -0,0 +1,53 @@
+#include "utils/check.h"
+
+#include <spdlog/spdlog.h>
+#include <cstdio>
+#include <cstdlib>
+
+#if KRG_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#endif
+
+namespace kryga::utils
+{
+
+bool
+is_debugger_attached()
+{
+#if KRG_PLATFORM_WIN32
+    return ::IsDebuggerPresent() != 0;
+#else
+    return false;
+#endif
+}
+
+[[noreturn]] void
+assert_fail(const char* expr, const char* file, int line)
+{
+    auto logger = spdlog::default_logger();
+    if (logger)
+    {
+        logger->critical("ASSERT FAILED: {} ({}:{})", expr, file, line);
+        logger->flush();
+    }
+    else
+    {
+        fprintf(stderr, "ASSERT FAILED: %s (%s:%d)\n", expr, file, line);
+        fflush(stderr);
+    }
+
+    if (is_debugger_attached())
+    {
+        KRG_DEBUGBREAK();
+    }
+
+#if KRG_PLATFORM_WIN32
+    TerminateProcess(GetCurrentProcess(), 3);
+#else
+    _exit(3);
+#endif
+}
+
+}  // namespace kryga::utils
--- libs/utils/public/include/utils/check.h
+++ libs/utils/public/include/utils/check.h
@@ -1,8 +1,38 @@
 #pragma once
 
-#include <cassert>
+#include "kryga_port/platform.h"
 
-#define KRG_check(condition, msg) assert((condition) && msg)
-#define KRG_never(msg) assert(false && msg)
+#if KRG_PLATFORM_WIN32
+#define KRG_DEBUGBREAK() __debugbreak()
+#elif defined(__clang__) || defined(__GNUC__)
+#define KRG_DEBUGBREAK() __builtin_trap()
+#else
+#define KRG_DEBUGBREAK() ((void)0)
+#endif
+
+namespace kryga::utils
+{
+[[noreturn]] void
+assert_fail(const char* expr, const char* file, int line);
+
+bool
+is_debugger_attached();
+}  // namespace kryga::utils
+
+#define KRG_check(condition, msg)                                                  \
+    do                                                                             \
+    {                                                                              \
+        if (!(condition))                                                          \
+        {                                                                          \
+            ::kryga::utils::assert_fail(#condition " — " msg, __FILE__, __LINE__); \
+        }                                                                          \
+    } while (0)
+
+#define KRG_never(msg)                                        \
+    do                                                        \
+    {                                                         \
+        ::kryga::utils::assert_fail(msg, __FILE__, __LINE__); \
+    } while (0)
+
 #define KRG_not_implemented KRG_never("Not Implemented!")
-#define KRG_deprecate KRG_never("Not Implemented")
+#define KRG_deprecate KRG_never("Deprecated!")
